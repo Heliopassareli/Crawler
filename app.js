e0bf329ed7bcd639d074da2c34c35e82ac75004f
Heliopassareli <30806320+Heliopassareli@users.noreply.github.com>
2018-11-11 15:43:50 -0200

    Saving

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ var fs = require('fs');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
+const cheerio = require('cheerio');
 
 global.document = document;
-var $ = jQuery = require('jquery')(window);
+var jQuery = require('jquery')(window);
 var newsDir = 'news/';
 
+let newsToGet = [],
+    newsUrl = [];
+
 let getNewsUrl = () => {
-    let news = {
+    console.log("Crawling to get New's Urls. ");
+    crawlerjs({
         interval: 1000,
         getSample: 'https://br.investing.com/news/forex-news/',
         get: 'https://br.investing.com/news/forex-news/',
@@ -20,8 +25,7 @@ let getNewsUrl = () => {
                 selector: '#leftColumn .largeTitle article > a',
                 callback: function(err,html,url,response) {
                     if(!err) {
-                        let data = [];
-                        $.each(html, function(i, item){
+                        jQuery.each(html, function(i, item){
                             let newsCollection = {},
                                 link = 'https://br.investing.com',
                                 fileName = item.attribs.href;
@@ -29,51 +33,97 @@ let getNewsUrl = () => {
                             newsCollection.id = item.parent.attribs['data-id'];
                             newsCollection.name = fileName.split('/')[3];
                             newsCollection.url = link + item.attribs.href;
-                            data.push(newsCollection);
+                            newsUrl.push(newsCollection);
                         });
 
-                        getNews(data);
                      }else {
                         console.log(err);
                     }
                 }
             }
         ]
+    });
+    setTimeout(getUrlCallback, 5000);
+}
+
+let getUrlCallback = () => {
+    console.log("Crawling to get New's Urls. done!");
+    console.log('Searching for duplicated urls in collection.');
+    
+    let currentUrls = [], delletions = 0;
+    newsUrl.forEach(function(news) {
+        let existInNewsUrl = false;
+        newsToGet.forEach(function(notice){
+            if(fs.existsSync(newsDir + notice.name + '.txt') || notice.id === news.id) {
+                delletions++;
+                existInNewsUrl = true;
+            }
+        });
+        if(!existInNewsUrl) currentUrls.push(news);
+    });
+
+    newsUrl = currentUrls;
+
+    if(newsUrl.length > 0) {
+        newsUrl.forEach(function(notice) { 
+            newsToGet.push(notice);
+        });
     }
-
-    crawlerjs(news);
+    console.log('Searching for duplicated urls in collection. done!');
+    console.log("We've got "+ newsUrl.length + " added and "+ delletions+ " removed.");
+    newsUrl = [];
+    verifyNewsContent();
 }
 
-let getUrlCallback = (data = []) => {
-    data.forEach(function(news) { if(fs.existsSync(newsDir + news.id + '.txt')) delete news; });
+let verifyNewsContent = () => {
+    console.log("Verifying NewsToGet collection. ");
+    if(newsToGet.length > 0) {
+        console.log("We have "+ newsToGet.length +" news to get.");
+        setTimeout(getNews, 500);
+    } else {
+        console.log("We have no news to get. Configuring new check in 60s.");
+        setTimeout(getNewsUrl, 60000);
+    }
+};
 
-    getNews(data);
-}
+let getNews = () => {
+    if(newsToGet.length > 0) {
+        console.log("Starting queue process to get the actual notice. Queue amount: "+ newsToGet.length);
 
-let getNews = (data) => {
-    crawlerjs({
-        interval: 1000,
-        getSample:'https://br.investing.com/news/forex-news/ilan-defende-cambio-flutuante-e-reservas-para-proteger-economia-brasileira-de-choques-externos-615769',
-        get: 'https://br.investing.com/news/forex-news/ilan-defende-cambio-flutuante-e-reservas-para-proteger-economia-brasileira-de-choques-externos-615769',
-        preview: 0,
-        extractors: [
-            {
-                selector: '#leftColumn .articlePage p',
-                callback: function(err,html,url,response) {
-                    if(!err) {
-                        let newsText = '';
-                        $.each(html, function(i, item){
-                            item.forEach(function(text){
-                                console.log(text.data);
-                            })
-                        });
-                    }else {
-                        console.log(err);
+        let currentNoticeConfig = newsToGet[newsToGet.length - 1];
+        newsToGet.pop();
+        crawlerjs({
+            interval: 1000,
+            getSample: currentNoticeConfig.url,
+            get: currentNoticeConfig.url,
+            preview: 0,
+            extractors: [
+                {
+                    selector: '#leftColumn',
+                    callback: function(err,html,url,response) {
+                        console.log('Crawling: '+ currentNoticeConfig.url);
+                        let articleText = '';
+                        if(!err) {
+                            let test = [];
+                            let { window } = new JSDOM();
+                            let { document }  = (new JSDOM(html)).window;
+                            let $ = require('jquery')(window);
+                            console.log($('.articlePage').text());
+                        }else {
+                            console.log(err);
+                        }
                     }
                 }
-            }
-        ]
-    });
+            ]
+        });
+        if(newsToGet.length > 0) {
+            console.log('Next search occurs in 10s.')
+            setTimeout(getNews, 10000);
+        }
+    } else {
+        console.log("We have no news to get. Configuring new check in 10s.");
+        setTimeout(getNewsUrl, 10000);
+    }
 }
 
-getNews();
+getNewsUrl();
--- news.js
+++ news.js
@@ -3,17 +3,10 @@ var cheerio = require('cheerio');
 var fs      = require('fs');
 
 let getNewsUrl = () => {
-    request('https://br.investing.com/news/forex-news/', function(err, res, body){
-        if(err) console.log('Error: ' + err);
-console.log(err);
-console.log(res);
-        let $ = cheerio.load(body);
-        $('#leftColumn .largeTitle article .textDiv a').each(function(){
-            let link = 'https://br.investing.com';
-            let url  = link + $(this).attr('href');
-            console.log(url);
-            //fs.appendFile('imdb.txt', title + ' - ' + rating + '\n');
-        });
+    request
+    .get('https://br.investing.com/news/forex-news/').on('response', function(response){
+        
+        console.log(response);
     });
 }
 
