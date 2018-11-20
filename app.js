d66200fea6f3cef5c058c7f061c41879aa09cdb8
helio <heliopassareli@gmail.com>
2018-11-20 00:32:05 -0200

    Saving changes

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ var fs = require('fs');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
-const cheerio = require('cheerio');
 
 global.document = document;
-var jQuery = require('jquery')(window);
-var newsDir = 'news/';
+var $ = require('jquery')(window);
+var newsDir = 'news/',
+    logFile = 'crawler.log';
 
 let newsToGet = [],
     newsUrl = [];
+
+let log = (content) => {
+    let dateNow = new Date(),
+        day = dateNow.getDate(),
+        month = dateNow.getMonth(),
+        year = dateNow.getFullYear(),
+        logMessage = '';
+
+    logMessage = `<${day}/${month}/${year} ${dateNow.getHours()}:${dateNow.getMinutes()} \n`;
+    logMessage += content + '\n';
+    logMessage += 'Log end... >';
+
+    fs.appendFile(logFile, logMessage, 'utf8', function(err){ });
+};
 
 let getNewsUrl = () => {
     console.log("Crawling to get New's Urls. ");
@@ -25,11 +39,11 @@ let getNewsUrl = () => {
                 selector: '#leftColumn .largeTitle article > a',
                 callback: function(err,html,url,response) {
                     if(!err) {
-                        jQuery.each(html, function(i, item){
+                        $.each(html, function(i, item){
                             let newsCollection = {},
                                 link = 'https://br.investing.com',
                                 fileName = item.attribs.href;
-                            
+
                             newsCollection.id = item.parent.attribs['data-id'];
                             newsCollection.name = fileName.split('/')[3];
                             newsCollection.url = link + item.attribs.href;
@@ -44,36 +58,43 @@ let getNewsUrl = () => {
         ]
     });
     setTimeout(getUrlCallback, 5000);
-}
+};
 
 let getUrlCallback = () => {
     console.log("Crawling to get New's Urls. done!");
     console.log('Searching for duplicated urls in collection.');
-    
+
     let currentUrls = [], delletions = 0;
     newsUrl.forEach(function(news) {
         let existInNewsUrl = false;
-        newsToGet.forEach(function(notice){
-            if(fs.existsSync(newsDir + notice.name + '.txt') || notice.id === news.id) {
+        if(newsToGet.length > 0) {
+            newsToGet.forEach(function(notice){
+                if(fs.existsSync(newsDir + notice.name + '.txt') || notice.id === news.id) {
+                    delletions++;
+                    existInNewsUrl = true;
+                }
+            });
+        } else {
+            if(fs.existsSync(newsDir + news.name + '.txt')) {
                 delletions++;
                 existInNewsUrl = true;
             }
-        });
+        }
         if(!existInNewsUrl) currentUrls.push(news);
     });
 
     newsUrl = currentUrls;
 
     if(newsUrl.length > 0) {
-        newsUrl.forEach(function(notice) { 
+        newsUrl.forEach(function(notice) {
             newsToGet.push(notice);
         });
     }
     console.log('Searching for duplicated urls in collection. done!');
-    console.log("We've got "+ newsUrl.length + " added and "+ delletions+ " removed.");
+    console.log("We've got "+ newsUrl.length + " added and "+ delletions+ " ignored.");
     newsUrl = [];
     verifyNewsContent();
-}
+};
 
 let verifyNewsContent = () => {
     console.log("Verifying NewsToGet collection. ");
@@ -89,7 +110,6 @@ let verifyNewsContent = () => {
 let getNews = () => {
     if(newsToGet.length > 0) {
         console.log("Starting queue process to get the actual notice. Queue amount: "+ newsToGet.length);
-
         let currentNoticeConfig = newsToGet[newsToGet.length - 1];
         newsToGet.pop();
         crawlerjs({
@@ -99,31 +119,63 @@ let getNews = () => {
             preview: 0,
             extractors: [
                 {
-                    selector: '#leftColumn',
+                    selector: '#leftColumn .articlePage',
                     callback: function(err,html,url,response) {
                         console.log('Crawling: '+ currentNoticeConfig.url);
-                        let articleText = '';
+                        let articleText = '',
+                            fileName = newsDir + currentNoticeConfig.name + '.txt';
                         if(!err) {
-                            let test = [];
-                            let { window } = new JSDOM();
-                            let { document }  = (new JSDOM(html)).window;
-                            let $ = require('jquery')(window);
-                            console.log($('.articlePage').text());
+                            console.log('Getting page content...');
+                            if(html.find('p').length > 0) {
+                                console.log('Element p found...');
+                                articleText = $.trim(html.find('p').text());
+                            } else {
+                                console.log('Element p not found...');
+                                articleText = $.trim(html.text());
+                            }
+
+                            if(articleText !== '') {
+                                fs.writeFile(fileName, articleText, 'utf8', function(err){
+                                   if(err) {
+                                       newsToGet.push(currentNoticeConfig);
+                                       console.log(`Error trying to write notice file: ${err}`);
+                                       console.log('Current notice pushed to NewsToGet ... Ok');
+                                   } else {
+                                       console.log(`Notice ${fileName} successfully acquired!`);
+                                   }
+
+                                    handleNextStep(true);
+                                });
+                            } else {
+                                newsToGet.push(currentNoticeConfig);
+                                console.log('Variable articleText was empty. Pushing back current noticeConfig...');
+                                handleNextStep();
+                            }
                         }else {
-                            console.log(err);
+                            newsToGet.push(currentNoticeConfig);
+                            console.log(`Error trying to access notice: ${err}`);
+                            console.log('Current notice pushed to NewsToGet ... Ok');
+                            handleNextStep();
                         }
                     }
                 }
             ]
         });
-        if(newsToGet.length > 0) {
-            console.log('Next search occurs in 10s.')
-            setTimeout(getNews, 10000);
-        }
+
     } else {
         console.log("We have no news to get. Configuring new check in 10s.");
-        setTimeout(getNewsUrl, 10000);
+        handleNextStep();
     }
-}
+};
+
+let handleNextStep = (finishGettingNews = false) => {
+    if(newsToGet.length > 0) {
+        console.log('Next notice extract occurs in 10s');
+        setTimeout(getNews, 10000);
+    } else {
+        console.log('No else news to get. Configuring next news verification for 60 from now on.');
+        setTimeout(getNewsUrl, 60000);
+    }
+};
 
 getNewsUrl();
